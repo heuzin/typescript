62c03d4e51df4fcc6ad4e767047d729d43864035
heuzin <matheusqs.arq@gmail.com>
2021-07-29 22:45:31 -0400

    compostiion

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,27 @@
-import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { MatchReader } from './MatchReader'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
-const reader = new MatchReader('football.csv')
-reader.read()
+// const reader = new MatchReader('football.csv')
+// reader.read()
+const csvFileReader = new CsvFileReader('football.csv')
 
-let manUnitedWins = 0;
+// Create an object that satisfies the 'DataReader' interface
+// Create an nstance of MatchReader and pass ni somtehing satisfying the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
-for(let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    };
-};
+let manUnitedWins = 0
 
-console.log(`Man United won ${manUnitedWins} games`);
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`)
+
+// Inheritance: IS A relationship
+// Compositoin: HAS A relationship
